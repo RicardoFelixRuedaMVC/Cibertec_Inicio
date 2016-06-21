91d8f554ed5fda12913b5ae510a1538652cd666a
ricardofelixruedaMVC <rickfelixr@gmail.com>
2016-06-20 22:32:54 -0500

    Razor

---
+++ WebDeveloper/Controllers/RazorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Controllers
+{
+    public class RazorController : Controller
+    {
+        // GET: Razor
+        public ActionResult Index()
+        {
+            var client = new Client()
+            {
+                id = 1,
+                Name = "Carlos",
+                LastName = "Perez"
+
+            };
+            return View();
+        }
+    }
+}
