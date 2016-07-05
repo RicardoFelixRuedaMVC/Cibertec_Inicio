1bb6605407a423ae05510a69cb76ea6b10c44ad6
ricardofelixruedaMVC <rickfelixr@gmail.com>
2016-07-04 21:15:14 -0500

    clase 12: injector II

--- WebDeveloper/Startup.cs
+++ WebDeveloper/Startup.cs
@@ -3,6 +3,8 @@ using Owin;
 using LightInject;
 using WebDeveloper.DataAccess;
 using WebDeveloper.Model;
+using System.Reflection;
+
 [assembly: OwinStartupAttribute(typeof(WebDeveloper.Startup))]
 namespace WebDeveloper
 {
@@ -11,10 +13,9 @@ namespace WebDeveloper
         public void Configuration(IAppBuilder app)
         {
             var container = new ServiceContainer();
-            container.Register<IDataAccess<Client>, ClientData>();
-           // container.RegisterAssembly(A)
-
-
+            //container.Register<IDataAccess<Client>, ClientData>();
+            container.RegisterAssembly(Assembly.GetExecutingAssembly());
+            container.RegisterAssembly("WebDeveloper.*.dll");
             container.RegisterControllers();
             container.EnableMvc();
 
